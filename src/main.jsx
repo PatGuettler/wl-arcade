7b37acaaeecdaf2c8bec25fa74dd88621b8f6ad0
Pat Guettler <patguettler@gmail.com>
2025-11-30 14:02:40 -0600

    Stop tracking files listed in gitignore. Added in hide pulldown bar function

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { StatusBar } from '@capacitor/status-bar';
+
+// Function to hide the status bar on mobile - was covering up game buttons
+const hideStatusBar = async () => {
+  try {
+    await StatusBar.hide();
+  } catch (err) {
+    console.log("Status bar plugin not available (web mode)");
+  }
+};
+
+hideStatusBar();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
