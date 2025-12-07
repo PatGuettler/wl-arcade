ef3c8867cdf368dbeaf12afa9f1bed1555224132
Pat Guettler <patguettler@gmail.com>
2025-12-06 21:57:12 -0600

    removing set level and logic checkout on 15 levels

--- src/games/cashCounter/index.jsx
+++ src/games/cashCounter/index.jsx
@@ -59,10 +59,7 @@ const CashCounterGame = ({
     if (next === target) {
       onSaveProgress(level, elapsedTime / 1000);
       setGameState("scoring");
-      setTimeout(
-        () => setGameState(level === 15 ? "victory" : "levelComplete"),
-        1000
-      );
+      setTimeout(() => setGameState("levelComplete"), 1000);
     } else if (next > target) {
       setGameState("scoring");
       setTimeout(() => setGameState("failed"), 1000);
